9445e77e2dab0709fd8606e6532e33e1f4fd79c2
senda-akiha <senda.akiha@gmail.com>
2014-02-28 23:29:05 +0900

    Add comment

--- flycheck-pos-tip.el
+++ flycheck-pos-tip.el
@@ -1,8 +1,37 @@
-;;; flycheck-pos-tip.el --- Display Flycheck errors using pos-tip
+;;; flycheck-pos-tip.el --- Flycheck errors display in tooltip
 
+;; Copyright (C) 2014  Akiha Senda
+
+;; Author: Akiha Senda <senda.akiha@gmail.com>
+;; URL: https://github.com/flycheck/flycheck-pos-tip
+;; Keywords: tools, convenience
+;; Version: 0.0.1
 ;; Package-Requires: ((flycheck "0.18") (pos-tip "0.4.5"))
 
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
+;; Add error message display method for Flycheck.
+
+;;;; Setup
+
+;; (eval-after-load 'flycheck
+;;   '(custom-set-variables
+;;    '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
 ;;; Code:
 
